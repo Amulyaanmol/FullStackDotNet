f2041bfcf5599df85c712cf72d8b2a1c679b4e67
Amulya <amulyaanmolg@gmail.com>
2022-03-07 15:33:57 +0530

    Variable creation and deletion

---
+++ IntroductionCSharp/Variables/Variable.cs
@@ -0,0 +1,19 @@
+﻿
+namespace IntroductionCSharp
+{
+    public static class Variable
+    {
+        static void Main()
+        {
+            string change; //create variable of type string
+            string message = "Enjoy the day!"; // Assign value to variable
+            change = "do you think soo?";
+            System.Console.WriteLine(message);
+            System.Console.WriteLine(change);
+            message = "yes."; // override variable
+            System.Console.WriteLine(message);
+        }
+    }
+}
+    
+
