92f37d96a8fbefa203b4dca533499cfef32302ce
Amulya <amulyaanmolg@gmail.com>
2022-03-07 15:36:35 +0530

    Checking with organizing files in a correct format

--- IntroductionCSharp/Variable.cs
+++ IntroductionCSharp/Variable.cs
@@ -1,4 +1,9 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace IntroductionCSharp
 {
     public static class Variable
@@ -15,5 +20,3 @@ namespace IntroductionCSharp
         }
     }
 }
-    
-
