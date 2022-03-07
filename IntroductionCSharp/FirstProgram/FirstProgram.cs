9fcc017ef828e868c8ad28c5c1599165a0f2a15c
Amulya <amulyaanmolg@gmail.com>
2022-03-07 15:30:08 +0530

    First Program has been added

---
+++ IntroductionCSharp/FirstProgram/FirstProgram.cs
@@ -0,0 +1,11 @@
+﻿
+namespace IntroductionCSharp
+{
+    public static class HeyProgram
+    {
+        static void Main() // method declaration with no return value
+        {
+            System.Console.WriteLine("Hey. My name is Ajith."); // writing output to console using System library writeline
+        }
+    }
+}
